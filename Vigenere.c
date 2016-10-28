5fa479d14ef991a0aaedd9854775f50d8770957e
iotvl <vladik2395@gmail.com>
2016-10-29 00:55:45 +0300

    Second commit

--- Vigenere.c
+++ Vigenere.c
@@ -4,48 +4,52 @@
 
 int main(int argc, string argv[])
 {
-    int l,j,o;
+    int length ,j,o;
     if (argc !=2)
     {
         printf("Usage: ./asciimath key");
         return 1;
     }
-    string k = argv[1];
-    for (j=0, l=strlen(k); j<l; j++)
+    string key = argv[1];
+    for (j=0, length=strlen(key); j<length; j++)
     {
-        if (k[j]<'A' || k[j]>'z') { printf("Usage: ./asciimath key"); return 1;}
+        if (key[j]<'A' || key[j]>'z') 
+		{ 
+			printf("Usage: ./asciimath key"); 
+			return 1;
+		}
     }
     string s = GetString();
     j=0;
     for (int i=0, n=strlen(s); i<n; i++)
     {
-        o=j%l;
+        o=j%length;
         if (s[i]>='A' && s[i]<='Z') 
         {
-            if(k[o]>='A' && k[o]<='Z')
+            if(key[o]>='A' && key[o]<='Z')
             {
-                s[i]=(s[i]-65+k[o]-65)%26;
+                s[i]=(s[i]-65+key[o]-65)%26;
                 s[i]=(s[i]+65);
                 j=j+1;
             }
             else
             {
-              s[i]=(s[i]-65+k[o]-97)%26;
+              s[i]=(s[i]-65+key[o]-97)%26;
               s[i]=(s[i]+65);
               j=j+1;  
             }
         }
         else if (s[i]>='a' && s[i]<='z') 
         {
-            if(k[o]>='A' && k[o]<='Z')
+            if(key[o]>='A' && key[o]<='Z')
             {
-               s[i]=(s[i]-97+k[o]-65)%26;
+               s[i]=(s[i]-97+key[o]-65)%26;
                s[i]=(s[i]+97);
                j=j+1;
             }
             else
             {
-               s[i]=(s[i]-97+k[o]-97)%26;
+               s[i]=(s[i]-97+key[o]-97)%26;
                s[i]=(s[i]+97);
                j=j+1;
             }
