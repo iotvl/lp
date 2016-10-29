6d856815f8e701fa123f24fa3f3efc51bff1cb9c
iotvl <vladik2395@gmail.com>
2016-10-29 18:45:28 +0300

    Seventh commit

--- Vigenere.c
+++ Vigenere.c
@@ -4,51 +4,51 @@
 
 int main(int argc, string argv[])
 {
-    if (argc !=2)
+	if (argc !=2)
 	{
 		printf("Usage: ./asciimath key");
 		return 1;
     }
-	string key = argv[1];
-	int length = strlen(key);
-    for (int index=0; index<length; index++)
-    {
+	string key = argv[1]; //get key
+	int keyLength = strlen(key);
+	for (int index=0; index<keyLength; index++) //validate key
+	{
 		if (key[index]<'A' || key[index]>'z') 
 		{ 
 			printf("Usage: ./asciimath key"); 
 			return 1;
 		}
-    }
-    string s = GetString();
-    for (int index=0, n=strlen(s); index<n; index++)
-    {
-        int modulo=index%length;
-        if (s[index]>='A' && s[index]<='Z') 
-        {
-            if(key[modulo]>='A' && key[modulo]<='Z')
-            {
-                s[index]=(s[index]-65+key[modulo]-65)%26;
-                s[index]=s[index]+65;
-            }
-            else
-            {
-              s[index]=(s[index]-65+key[modulo]-97)%26;
-              s[index]=s[index]+65;
-            }
-        }
-        else if (s[index]>='a' && s[index]<='z') 
-        {
-            if(key[modulo]>='A' && key[modulo]<='Z')
-            {
-               s[index]=(s[index]-97+key[modulo]-65)%26;
-               s[index]=s[index]+97;
-            }
-            else
-            {
-               s[index]=(s[index]-97+key[modulo]-97)%26;
-               s[index]=s[index]+97;
-            }
-        }
-    }
-    printf("%s\n",s);
+	}
+	string String = GetString(); // get String
+	for (int index=0, stringLength=strlen(String); index<stringLength; index++) //encrypt characters
+	{
+		int modulo=index%keyLength;
+		if (String[index]>='A' && String[index]<='Z') 
+		{
+			if(key[modulo]>='A' && key[modulo]<='Z')
+			{
+				String[index]=(String[index]-'A'+key[modulo]-'A')%26;
+				String[index]=String[index]+'A';
+			}
+			else
+			{
+				String[index]=(String[index]-'A'+key[modulo]-'a')%26;
+				String[index]=String[index]+'A';
+			}
+		}
+		else if (String[index]>='a' && String[index]<='z') 
+		{
+			if(key[modulo]>='A' && key[modulo]<='Z')
+			{
+				String[index]=(String[index]-'a'+key[modulo]-'A')%26;
+				String[index]=String[index]+'a';
+			}
+			else
+			{
+				String[index]=(String[index]-'a'+key[modulo]-'a')%26;
+				String[index]=String[index]+'a';
+			}
+		}
+	}
+	printf("%s\n",String);
 }
